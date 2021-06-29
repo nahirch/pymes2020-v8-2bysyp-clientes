e6b6e7c34ab5d2f6d36ec4b29d181ddd26496429
nahirch <nahirdelosangeleschoque@gmail.com>
2021-06-29 17:12:25 -0300

    3 files modified, 1 added

--- src/app/components/clientes/clientes.component.ts
+++ src/app/components/clientes/clientes.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Cliente } from "../../models/cliente";
+import { ClientesService } from "../../services/clientes.service";
+import { ModalDialogService } from "../../services/modal-dialog.service";
 
 @Component({
   selector: 'app-clientes',
@@ -6,10 +10,111 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./clientes.component.css']
 })
 export class ClientesComponent implements OnInit {
+  Titulo = "Clientes";
+  TituloAccionABMC = {
+    A: "(Agregar)",
+    B: "(Eliminar)",
+    M: "(Modificar)",
+    C: "(Consultar)",
+    L: "(Listado)"
+  };
+  AccionABMC = "L"; // inicialmente inicia en el listado de articulos (buscar con parametros)
+  Mensajes = {
+    SD: " No se encontraron registros...",
+    RD: " Revisar los datos ingresados..."
+  };
 
-  constructor() { }
+  Lista: Cliente[] = [];
+  RegistrosTotal: number;
+  SinBusquedasRealizadas = true;
+
+  Pagina = 1; // inicia pagina 1
+
+  // opciones del combo activo
+  OpcionesActivo = [
+    { Id: null, Nombre: "" },
+    { Id: true, Nombre: "SI" },
+    { Id: false, Nombre: "NO" }
+  ];
+
+  FormFiltro: FormGroup;
+  FormReg: FormGroup;
+  submitted = false;
+
+  constructor(
+    public formBuilder: FormBuilder,
+    private clientesService: ClientesService,
+    private modalDialogService: ModalDialogService
+  ) {}
 
   ngOnInit() {
+    this.FormReg = this.formBuilder.group({
+      IdCliente: [0],
+      Nombre: [
+        "",
+        [Validators.required, Validators.minLength(4), Validators.maxLength(40)]
+      ],
+      Cuit: [null, [Validators.required, Validators.pattern("[0-9]{1,7}")]],
+      TieneTrabajo: [true]
+    });
+  }
+
+  Agregar() {
+    this.AccionABMC = "A";
+    this.FormReg.reset({ Activo: true });  // markAsUntouched
+    this.submitted = false;
+    //this.FormReg.markAsPristine();
+    //this.FormReg.markAsUntouched();
+  }
+
+  // Buscar segun los filtros, establecidos en FormReg
+  Buscar() {
+    this.clientesService
+      .get()
+      .subscribe((res: any) => {
+        this.Lista = res;
+        this.RegistrosTotal = res.RegistrosTotal;
+      });
+  }
+
+
+  //Consultar(Dto) {
+    //this.BuscarPorId(Dto, "C");
+  //}
+
+
+  // grabar tanto altas como modificaciones
+  Grabar() {
+    this.submitted = true;
+    // verificar que los validadores esten OK
+    if (this.FormReg.invalid) {
+      return;
+    }
+
+    //hacemos una copia de los datos del formulario, para modificar la fecha y luego enviarlo al servidor
+    const itemCopy = { ...this.FormReg.value };
+
+  
+
+    // agregar post
+    if (itemCopy.IdArticulo == 0 || itemCopy.IdArticulo == null) {
+      itemCopy.IdArticulo = 0;
+      this.clientesService.post(itemCopy).subscribe((res: any) => {
+        this.Volver();
+        this.modalDialogService.Alert("Registro agregado correctamente.");
+        this.Buscar();
+      });
+    }
+  }
+
+
+  // Volver desde Agregar/Modificar
+  Volver() {
+    this.AccionABMC = "L";
+  }
+
+  ImprimirListado() {
+    this.modalDialogService.Alert("Sin desarrollar...");
   }
 
 }
